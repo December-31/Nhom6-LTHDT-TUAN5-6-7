4569a723dec91631516308afaeff21ad28463523
Trương Văn Cường <vancuong31122k6@gmail.com>
2026-03-13 21:40:03 +0700

    Add files via upload

---
+++ bai1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    internal class bai1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ho ten: Truong Van Cuong   MaSV: 2415053122304");
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var result = numbers.Where(n => n % 2 == 0);
+
+            Console.WriteLine("Cac so chan: " + string.Join(", ", result));
+            Console.ReadLine();
+        }
+    }
+}
